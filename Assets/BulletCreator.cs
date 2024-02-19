891e4a7d22bd8b19a8b7333ab90f28e71ecbad05
Evgenyi <VRN.Ocenka@gmail.com>
2024-02-19 14:45:17 +0300

    Fire

--- Assets/BulletCreator.cs
+++ Assets/BulletCreator.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,6 +11,7 @@ public class BulletCreator : MonoBehaviour
     [SerializeField] private GameObject _gun;
     [SerializeField] private Transform _bulletspawn;
     [SerializeField] private Image _strangeImage;
+    [SerializeField] private TMP_Dropdown _dropdown;
     public int bulletAddVelocity;
     private int _velocity;
     public bool _isPress;
@@ -21,18 +23,40 @@ public class BulletCreator : MonoBehaviour
     {
         _strangeImage.fillAmount = 0;
         IsMobile = Application.isMobilePlatform;
+        _dropdown.options[0].text = 10.ToString();
+        _dropdown.captionText.text = 10.ToString();
+        _dropdown.options[1].text = 5.ToString();
+        _dropdown.options[2].text = 1.ToString();
     }
 
-    public void Fire(InputAction.CallbackContext context)
-    {
-       if (IsMobile) return;
-        Debug.Log($"Context {context.phase}");
-        if (context.started)
-        {
-            _isPress = true;
-        }
+    // public void Fire(InputAction.CallbackContext context)
+    // {
+    //    if (IsMobile) return;
+    //     Debug.Log($"Context {context.phase}");
+    //     if (context.started)
+    //     {
+    //         _isPress = true;
+    //     }
+    //
+    //     if (context.canceled)
+    //     {
+    //         GameObject newBullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+    //         newBullet.GetComponent<Rigidbody>().velocity = transform.forward * _velocity;
+    //         _isPress = false;
+    //         _velocity = 0;
+    //         _strangeImage.fillAmount = 0;
+    //     }
+    // }
 
-        if (context.canceled)
+    public void FireMobile()
+    {
+        if (!IsMobile && Input.GetMouseButtonDown(0))
+            _isPress = true;
+    }
+
+    public void FireMobileUp()
+    {
+        if (_isPress)
         {
             GameObject newBullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
             newBullet.GetComponent<Rigidbody>().velocity = transform.forward * _velocity;
@@ -42,20 +66,6 @@ public class BulletCreator : MonoBehaviour
         }
     }
 
-    public void FireMobile()
-    {
-        _isPress = true;
-    }
-
-    public void FireMobileUp()
-    {
-        GameObject newBullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
-        newBullet.GetComponent<Rigidbody>().velocity = transform.forward * _velocity;
-        _isPress = false;
-        _velocity = 0;
-        _strangeImage.fillAmount = 0;
-    }
-    
     private void Update()
     {
         if (_isPress)
